c5b8602d9619b0909337bf3d40da69417551c48b
Artur Leite <arturleite164@gmail.com>
2022-11-30 21:44:46 +0000

    documentacao do vectorracer.py

--- src/vectorracer.py
+++ src/vectorracer.py
@@ -3,12 +3,25 @@ from queue import Queue
 from random import randint
 
 class VectorRacer:
+    """
+    Construtor VectorRacer
+    """
     def __init__(self):
+        #Mapa
         self.map = []
+        #posicao inicial('P') do mapa
         self.posicao_inicial = Node((0, 0))
+        #posicoes finais('F') do mapa
         self.posicoes_finais = []
+        #representação do grafo pelo mapa
         self.graph = Graph(True)
 
+    """
+    linhas: int: numero de linhas que novo mapa deve ter
+    colunas: int: numero de colunas que novo mapa deve ter
+
+    Funcao que gera um mapa recebendo um numero de linhas e colunas, o mapa gerado será random
+    """
     def gen_map(self, linhas: int, colunas: int):
         valid = False
         while not valid:
@@ -42,8 +55,9 @@ class VectorRacer:
                     print(line)
                 valid = True
                 
-                
-
+    """
+    Função que transforma um caracter do mapa para um inteiro(codigo de caracter)
+    """
     def __from_char_to_int__(self, sec: str) -> int:
         if sec == 'X':
             return 0
@@ -57,6 +71,9 @@ class VectorRacer:
             return 0
              
 
+    """
+    Funcao que gera uma matriz a partir do mapa, colocando cara caracter pelo codigo correspondente
+    """
     def get_map_as_matrix(self) -> list[list[int]]:
         mat = []
         for (i, line) in enumerate(self.map):
@@ -68,6 +85,9 @@ class VectorRacer:
         return mat
                     
 
+    """
+    Funcao que calcula as posicoes finais do mapa, encontrando os caracteres F no mapa
+    """
     def calcula_posicoes_finais(self):
         resultados: list[tuple[int, int]] = []
         for (i, line) in enumerate(self.map):
@@ -77,6 +97,9 @@ class VectorRacer:
         return resultados
             
 
+    """
+    Funcao que gera o grafo a partir do mapa definido, explorando as opções a partir de um estado 
+    """
     def load_graph(self):
         nodo_inicial = self.posicao_inicial
         queue: Queue[tuple[Node, int]] = Queue()
@@ -96,6 +119,9 @@ class VectorRacer:
                     queue.put(estado)
 
 
+    """
+    Funcao que calcula os estados possiveis a partir de um nodo
+    """
     def estados_possiveis(self, estado: Node) -> set[tuple[Node, int]]:
         if not self.check_bounds(list(estado.get_posicao())):
             return set()
@@ -119,6 +145,10 @@ class VectorRacer:
 
         return estados
 
+
+    """
+    Funcao que encontra a posicao inicial no mapa, ou seja, o caracter P
+    """
     def calcula_posicao_inicial(self):
         posicao_inicial = (0, 0)
         found = False
@@ -136,14 +166,21 @@ class VectorRacer:
 
         self.posicao_inicial: Node = Node(posicao_inicial)
 
+        """
+        Funcao que verifica se uma posicao se encontra nos limites do mapa
+        """
     def check_bounds(self, position: list[int]) -> bool:
             return position[0] >= 0 and position[0] < len(self.map) and position[1] >= 0 and position[1] < len(self.map[0])
 
+
+    """
+    Funcao que testa se de uma posicao até outra com uma determinada velocidade colide ou não com uma parede
+    A Funcao retorna (-1, -1) se nao tiver encontrado o final do mapa, e retorna a posicao final caso tenha encontrado um 'F' pelo caminho
+    """
     def check_colision(self, posicao_atual: tuple[int, int], velocidade: tuple[int, int], posicao_final: tuple[int, int]) -> tuple[int, int] | None:
         velocidade_atual = list(velocidade)
         current_position = list(posicao_atual)
 
-        index = 0
         while current_position[0] != posicao_final[0] or current_position[1] != posicao_final[1]:
             if (self.map[current_position[0]][current_position[1]] == 'F'):
                 return (current_position[0], current_position[1])
@@ -199,25 +236,34 @@ class VectorRacer:
                 
         return (-1, -1)
             
+    """
+    Funcao que calcula um par de Nodo e custo a partir de um estado aplicado a uma determinada aceleracao
+    """
     def prox_posicao(self, estado: Node, aceleracao: tuple[int, int]) -> tuple[Node, int]:
         posicao_atual = estado.get_posicao()
         velocidade_atual = estado.get_velocidade()
         velocidade_nova = (velocidade_atual[0] + aceleracao[0], velocidade_atual[1] + aceleracao[1])
         posicao_nova = (posicao_atual[0] + velocidade_nova[0], posicao_atual[1] + velocidade_nova[1])
         custo = 1
+        #Caso a nova posicao seja exatamente igual à anterior, ou seja nao possui velocidade nenhuma
         if posicao_nova == posicao_atual:
             return (Node(posicao_nova), 0)
         resultado = self.check_colision(posicao_atual, velocidade_nova, posicao_nova)
+        #Houve alguma colisao
         if resultado is None:
             posicao_nova = posicao_atual
             velocidade_nova = (0, 0)
             custo = 25
+        #caso em que no check_colision encontrou uma posicao final do mapa
         elif resultado != (-1, -1):
             posicao_nova = resultado
             velocidade_nova = (0, 0)
         estado_novo = Node(posicao_nova, velocidade_nova)
         return (estado_novo, custo)
         
+    """
+    Funcao que recebe um caminho para um ficheiro de texto com a localização do mapa, dando load ao mesmo
+    """
     def load_map_from_file(self, file: str):
         self.map: list[str] = []
         with open(file) as f:
@@ -237,8 +283,14 @@ class VectorRacer:
         value += ");"
         return value
 
+    """
+    Funcao que calcula o caminho através do algoritmo DFS 
+    """
     def dfs(self):
         return self.graph.dfs(self.posicao_inicial, self.posicoes_finais)
 
+    """
+    Funcao que calcula um caminho através do algoritmo BFS
+    """
     def bfs(self):
         return self.graph.bfs(self.posicao_inicial, self.posicoes_finais)
